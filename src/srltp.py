4b044b58dae0ff0a03d51ac9e8672e204e5b76c2
rodrigotiscareno <rodrigotiscareno1@gmail.com>
2024-07-29 14:14:26 -0400

    add our data

--- src/srltp.py
+++ src/srltp.py
@@ -1,19 +1,90 @@
 from gurobipy import Model, GRB, quicksum
 import sys
+import random
+import numpy as np
 
-# Example data (you will need to provide actual data)
-V = [1, 2, 3]  # List of retailer nodes
+
+V = [1, 2, 3, 4, 5, 6, 7]
 V_0 = [0] + V  # List of nodes including the depot
 
-V_plus = [1]  # List of surplus nodes
-V_minus = [2, 3]  # List of deficit nodes
-Q_max = 10  # Vehicle capacity
-T_max = 100  # Maximum tour duration
-c = {(i, j): 1 for i in V_0 for j in V_0}  # Cost matrix (example values)
-p = {i: 1 for i in V}  # Profit or loss per unit at location i
-q = {i: 5 for i in V_0}  # Quantity balance at location i
+V_plus = [1, 5, 6]  # List of surplus nodes
+V_minus = [2, 3, 4, 7]  # List of deficit nodes
+Q_max = 44  # Vehicle capacity
+T_max = 8400  # Maximum tour duration
+c = {
+    (0, 0): 0,
+    (0, 1): 9.31,
+    (1, 0): 9.31,
+    (0, 2): 32.10,
+    (2, 0): 32.10,
+    (0, 3): 32.74,
+    (3, 0): 32.74,
+    (0, 4): 30.01,
+    (4, 0): 30.01,
+    (0, 5): 24.56,
+    (5, 0): 24.56,
+    (0, 6): 10.11,
+    (6, 0): 10.11,
+    (0, 7): 21.83,
+    (7, 0): 21.83,
+    (1, 1): 0,
+    (1, 2): 31.30,
+    (2, 1): 31.30,
+    (1, 3): 31.94,
+    (3, 1): 31.94,
+    (1, 4): 33.54,
+    (4, 1): 33.54,
+    (1, 5): 29.85,
+    (5, 1): 29.85,
+    (1, 6): 23.11,
+    (6, 1): 23.11,
+    (1, 7): 17.49,
+    (7, 1): 17.49,
+    (2, 2): 0,
+    (2, 3): 8.19,
+    (3, 2): 8.19,
+    (2, 4): 22.95,
+    (4, 2): 22.95,
+    (2, 5): 42.21,
+    (5, 2): 42.21,
+    (2, 6): 31.94,
+    (6, 2): 31.94,
+    (2, 7): 45.74,
+    (7, 2): 45.74,
+    (3, 3): 0,
+    (3, 4): 22.31,
+    (4, 3): 22.31,
+    (3, 5): 41.57,
+    (5, 3): 41.57,
+    (3, 6): 40.45,
+    (6, 3): 40.45,
+    (3, 7): 45.10,
+    (7, 3): 45.10,
+    (4, 4): 0,
+    (4, 5): 33.54,
+    (5, 4): 33.54,
+    (4, 6): 38.84,
+    (6, 4): 38.84,
+    (4, 7): 43.34,
+    (7, 4): 43.34,
+    (5, 5): 0,
+    (5, 6): 13,
+    (6, 5): 13,
+    (5, 7): 36.27,
+    (7, 5): 36.27,
+    (6, 6): 0,
+    (6, 7): 28.89,
+    (7, 6): 28.89,
+    (7, 7): 0,
+}
+p = {1: 156, 2: 102, 3: 219, 4: 100, 5: 91, 6: 200, 7: 91}
+q = {i: 0 for i in V_0}
+for i in V_plus:
+    q[i] = random.randint(1, 12)
+for i in V_minus:
+    q[i] = -random.randint(1, 12)
 
-F = 5  # Fixed cost per vehicle
+F = 400  # Fixed cost per vehicle
 
 M = sys.maxsize  # Big M value
 
